c785730bad024feaf6095a8b8f82980729d9f6ce
Jaechan-So <jaechan331@gmail.com>
2022-08-14 17:27:19 +0900

    Set development configuration - Reference: Google Typescript Style, https://github.com/google/gts

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
+};
